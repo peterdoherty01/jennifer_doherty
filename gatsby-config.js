b97d8713a9631a87227359ba259d9af3878100b4
Peter Doherty <peter@computationhub.com>
2019-01-08 10:04:10 -0500

    add youtube plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,10 +50,29 @@ module.exports = {
   plugins: [
     `gatsby-plugin-styled-jsx`, // the plugin's code is inserted directly to gatsby-node.js and gatsby-ssr.js files
     `gatsby-plugin-styled-jsx-postcss`, // as above
+    "gatsby-remark-embed-video",
+    "gatsby-remark-responsive-iframe",
     {
       resolve: `gatsby-plugin-layout`,
       options: {
         component: require.resolve(`./src/layouts/`)
+      }
+    },
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [
+        {
+          resolve: "gatsby-remark-embed-video",
+          options: {
+            width: 800,
+            ratio: 1.77, // Optional: Defaults to 16/9 = 1.77
+            height: 400, // Optional: Overrides optional.ratio
+            related: false, //Optional: Will remove related videos from the end of an embedded YouTube video.
+            noIframeBorder: true //Optional: Disable insertion of <style> border: 0
+          }
+        }
+        ]
       }
     },
     {
