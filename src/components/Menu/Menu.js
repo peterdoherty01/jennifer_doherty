0ef884ac3c61583e24fd9d002a1af536d56cda88
Peter Doherty <peter@computationhub.com>
2019-01-08 11:32:18 -0500

    retry rm algolia search

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 require("core-js/fn/array/from");
 
 import { FaHome } from "react-icons/fa/";
-import { FaSearch } from "react-icons/fa/";
+// import { FaSearch } from "react-icons/fa/";
 import { FaEnvelope } from "react-icons/fa/";
 import { FaTag } from "react-icons/fa/";
 
@@ -25,8 +25,8 @@ class Menu extends React.Component {
     this.items = [
       { to: "/", label: "Home", icon: FaHome },
       { to: "/category/", label: "Categories", icon: FaTag },
-      { to: "/search/", label: "Search", icon: FaSearch },
-      ...pages,
+      // { to: "/search/", label: "Search", icon: FaSearch },
+      // ...pages,
       { to: "/contact/", label: "Contact", icon: FaEnvelope }
     ];
 
